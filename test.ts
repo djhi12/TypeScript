2d51b33540532fca3a9feeaf066f60146ff2a560
djhi12 <ido20002@byui.edu>
2024-01-31 14:32:40 +0800

    Initial commit

---
+++ test.ts
@@ -0,0 +1,29 @@
+// import { PrismaClient } from '@prisma/client'
+
+// const prisma = new PrismaClient()
+
+// async function main() {
+//   const newUser = await prisma.user.create({
+//     data: {
+//       name: 'Alice',
+//       email: 'alice@prisma.io',
+//       posts: {
+//         create: { title: 'Hello World' },
+//       },
+//       profile: {
+//         create: { bio: 'I like turtles' },
+//       },
+//     },
+//   })
+//   console.log(newUser)
+// }
+
+// main()
+//   .catch(e => {
+//     throw e
+//   })
+//   .finally(async () => {
+//     await prisma.$disconnect()
+//   })
+
+
