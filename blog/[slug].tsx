305fa7f9ca46b8b6b581c66d86390f41dac4287a
djhi12 <ido20002@byui.edu>
2024-01-31 13:55:46 +0800

    Initial commit

---
+++ blog/[slug].tsx
@@ -0,0 +1,13 @@
+// import { GetStaticProps, GetStaticPaths, GetServerSideProps } from 'next'
+ 
+// export const getStaticProps = (async (context) => {
+//   // ...
+// }) satisfies GetStaticProps
+ 
+// export const getStaticPaths = (async () => {
+//   // ...
+// }) satisfies GetStaticPaths
+ 
+// export const getServerSideProps = (async (context) => {
+//   // ...
+// }) satisfies GetServerSideProps
